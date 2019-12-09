f3d45916c9cf938e0d4b9bf4b55b980ca7570a89
Colin Cerveaux <colincerveaux@gmail.com>
2019-12-09 16:38:18 +0100

    fix app.tests 2

--- src/test/java/fr/dta/ovg/ApplicationTest.java
+++ src/test/java/fr/dta/ovg/ApplicationTest.java
@@ -6,10 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
  class ApplicationTests {
-    class Onvageeker1ApplicationTests {
 
             @Test
             void contextLoads() {
             }
-    }
 }
