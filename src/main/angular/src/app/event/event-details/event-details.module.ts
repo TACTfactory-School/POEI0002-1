16a8d24f07a7869b889cb63d746bf9cbcb0d26c0
Samy Moussaoui <samy.moussaoui.1@gmail.com>
2019-12-10 12:08:35 +0100

    Stylize the event detail

--- src/main/angular/src/app/event/event-details/event-details.module.ts
+++ src/main/angular/src/app/event/event-details/event-details.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EventDetailsComponent } from './event-details.component';
 import { LoaderModule } from 'src/app/shared/loader/loader.module';
-import { MatCardModule } from '@angular/material';
+import { MatCardModule, MatToolbar, MatToolbarModule } from '@angular/material';
 import { RouterModule } from '@angular/router';
+import { MaterialModule } from 'src/app/shared/material/material.module';
+import { OvgFormsModule } from 'src/app/shared/forms/ovg-forms.module';
 
 @NgModule({
   declarations: [EventDetailsComponent],
@@ -11,7 +13,10 @@ import { RouterModule } from '@angular/router';
     CommonModule,
     MatCardModule,
     RouterModule,
-    LoaderModule
+    LoaderModule,
+    MaterialModule,
+    MatToolbarModule,
+    OvgFormsModule
   ],
   exports: [EventDetailsComponent]
 })
