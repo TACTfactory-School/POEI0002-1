25ed410916c71a45ac899174474b1e26e10acf5b
Colin Cerveaux <colincerveaux@gmail.com>
2019-12-13 11:49:02 +0100

    initialize user detail before pull request

--- src/main/angular/src/app/user/user-details/user-details.module.ts
+++ src/main/angular/src/app/user/user-details/user-details.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserDetailsComponent } from './user-details.component';
+import { LoaderModule } from 'src/app/shared/loader/loader.module';
+import { MaterialModule } from 'src/app/shared/material/material.module';
+import { MatToolbarModule } from '@angular/material';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [UserDetailsComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    LoaderModule,
+    MaterialModule,
+    MatToolbarModule,
+    RouterModule
   ],
   exports: [UserDetailsComponent]
 })
