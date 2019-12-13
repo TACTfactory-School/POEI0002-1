2f111e94d6f9e030d5d016604913d4949c179759
Colin Cerveaux <colincerveaux@gmail.com>
2019-12-13 15:25:46 +0100

    User Details implements with description list (no scss)

--- src/main/angular/src/app/user/user-list/user-list.module.ts
+++ src/main/angular/src/app/user/user-list/user-list.module.ts
@@ -5,6 +5,7 @@ import { OvgFormsModule } from 'src/app/shared/forms/ovg-forms.module';
 import { OvgTablesModule } from 'src/app/shared/tables/ovg-tables.module';
 import { LoaderModule } from 'src/app/shared/loader/loader.module';
 import { PipesModule } from 'src/app/shared/pipes/pipes.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { PipesModule } from 'src/app/shared/pipes/pipes.module';
     OvgTablesModule,
     OvgFormsModule,
     LoaderModule,
+    RouterModule,
     PipesModule
   ],
   exports: [
