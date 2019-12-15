c90205b5d738b81f9824dd6a1105ed7bbd61d224
Colin Cerveaux <colincerveaux@gmail.com>
2019-12-15 16:28:06 +0100

    try to fix user preferences toggle onChange

--- src/main/angular/src/app/user/user-preferences/user-preferences.component.ts
+++ src/main/angular/src/app/user/user-preferences/user-preferences.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
-import { MatDialog } from '@angular/material';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { MatDialog, MatSlideToggleChange } from '@angular/material';
 import { Router } from '@angular/router';
 
 @Component({
@@ -12,11 +12,23 @@ export class UserPreferencesComponent implements OnInit {
 
   preferences: FormGroup;
   isModal: boolean;
-  checked = false;
 
-  constructor(public dialog: MatDialog, private router: Router) { }
+  // Hide label manual TODO refacto ?
+  hideBirthdate: string;
+  hideMail: string;
+  hideJob: string;
+  hideGender: string;
+  hideStatus: string;
+
+  @Input() checked: boolean;
+  id: number;
+
+  constructor(public dialog: MatDialog, private router: Router, private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.preferences = this.fb.group({
+      hide: new FormControl('', Validators.required)
+    })
   }
 
   onSubmit() { }
@@ -30,4 +42,23 @@ export class UserPreferencesComponent implements OnInit {
     this.router.navigate(['user/me']);
   }
 
+  onChange(ob: MatSlideToggleChange, id: number) {
+    if (ob.checked) {
+      switch (id) {
+        case 3: this.hideMail = 'Afficher';
+                break;
+        case 4: this.hideBirthdate = 'Afficher';
+                break;
+        case 5: this.hideJob = 'Afficher';
+                break;
+        case 6: this.hideMail = 'Afficher';
+                break;
+        case 7: this.hideStatus = 'Afficher';
+                break;
+      }
+    } else {
+      this.hideMail = this.hideBirthdate = this.hideJob = this.hideMail = this.hideStatus = 'Masquer';
+    }
+      //this.hideText = ob.source ? 'Afficher' : 'Masquer';
+  }
 }
