b17b399cd3f5b8406b1219df2b88096081e9e73f
Colin Cerveaux <colincerveaux@gmail.com>
2019-12-16 12:36:28 +0100

    user/me view OK with matCard

--- src/main/angular/src/app/user/user-profile/user-profile.module.ts
+++ src/main/angular/src/app/user/user-profile/user-profile.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserProfileComponent } from './user-profile.component';
+import { LoaderModule } from 'src/app/shared/loader/loader.module';
+import { MatToolbarModule, MatCardModule } from '@angular/material';
+import { PipesModule } from 'src/app/shared/pipes/pipes.module';
+import { MaterialModule } from 'src/app/shared/material/material.module';
 
 @NgModule({
   declarations: [ UserProfileComponent ],
-  imports: [ CommonModule ],
+  imports: [
+    CommonModule,
+    LoaderModule,
+    MatToolbarModule,
+    MaterialModule,
+    MatCardModule,
+    PipesModule
+ ],
   exports: [ UserProfileComponent ]
 })
 export class UserProfileModule { }
