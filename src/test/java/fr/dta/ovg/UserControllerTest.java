247d63e6ffdb8c67aacf50daf462dedf094b8b1e
Fabrice Reigner <fabricereigner49@msn.com>
2019-12-19 21:29:51 +0100

    delete useless import

--- src/test/java/fr/dta/ovg/UserControllerTest.java
+++ src/test/java/fr/dta/ovg/UserControllerTest.java
@@ -1,13 +1,5 @@
 package fr.dta.ovg;
 
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
-
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.assertEquals;
