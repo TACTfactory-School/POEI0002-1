dc6d9b20780f3393c8aa98bf4ea837c3f3531c0d
Colin Cerveaux <colincerveaux@gmail.com>
2019-12-20 11:41:49 +0100

    new attributes and last fixtures for FRONT mock

---
+++ src/main/java/fr/dta/ovg/fixtures/LanguageFixtureService.java
@@ -0,0 +1,109 @@
+/* Language Fixture Service class.
+ * @author Colin Cerveaux @C-ambium
+ * Define the entity User
+ * License : ©2019 All rights reserved.*/
+
+package fr.dta.ovg.fixtures;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.stream.Stream;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import fr.dta.ovg.entities.Language;
+import fr.dta.ovg.entities.LanguageLevel;
+import fr.dta.ovg.entities.User;
+import fr.dta.ovg.entities.UserLanguage;
+import fr.dta.ovg.exceptions.NotFoundException;
+import fr.dta.ovg.repositories.LanguageRepository;
+import fr.dta.ovg.repositories.UserLanguageRepository;
+import fr.dta.ovg.services.LanguageCrudService;
+import fr.dta.ovg.services.user.UserCrudServiceImpl;
+
+/** This class initialize DB with initials languages fixtures data. */
+@Component
+@Profile("!prod")
+public class LanguageFixtureService  extends FixtureCheck<LanguageRepository>{
+
+    /** Link to Language CRUD Service. */
+    private final LanguageCrudService langService;
+
+    /** Link to UserLanguage Repository. */
+    private final UserLanguageRepository userLanguageRepo;
+
+    /** Link to User CRUD Service. */
+    private final UserCrudServiceImpl userService;
+
+    /** Declare new Random object.*/
+    private final Random rand = new Random();
+
+    /** LanguageFixtureService Constructor. Initialize Language CRUD service.
+     * @param langService : @see LanguageCrudService.
+     * @param userLanguageRepo : @see UserLanguageRepository.
+     * @param userService : @see UserCrudServiceImpl.*/
+    public LanguageFixtureService(
+            @Autowired final LanguageCrudService langService,
+            @Autowired final UserLanguageRepository userLanguageRepo,
+            @Autowired final UserCrudServiceImpl userService) {
+        this.langService = langService;
+        this.userLanguageRepo = userLanguageRepo;
+        this.userService = userService;
+    }
+
+
+    /** Insert initial data - Create-drop mode will erasing old data in the DB at every run.
+     * @throws NotFoundException */
+    @Override
+    protected void loadIfNoData() throws NotFoundException {
+
+        Stream.of("FRANÇAIS", "ENGLISH", "DEUTSCH", "ITALIANO", "ESPAÑOL", "中国的", "ไทย", "РУССКИЙ")
+            .forEach(this::build);
+
+        this.buildUserLanguage(
+                this.LanguageLevelStore().get(rand.nextInt(4)),
+                this.langService.getOne(rand.nextInt(7) + 1),
+                this.userService.getOne(rand.nextInt(99) + 1));
+    }
+
+    /** Build Language Function.
+     * @param label : the language label.*/
+    private void build(final String label) {
+
+        final Language language = new Language();
+
+        language.setLabel(label);
+
+        langService.create(language);
+    }
+
+    /** Build UserLanguage Join table Function.
+     * @param label : the language label.*/
+    private void buildUserLanguage(final LanguageLevel level, final Language language, final User user) {
+
+        final UserLanguage userLang = new UserLanguage();
+
+        userLang.setLevel(level);
+        userLang.setLanguage(language);
+        userLang.setUser(user);
+
+        userLanguageRepo.save(userLang);
+    }
+
+    /** User Language Level Storage Function.
+     * @return List of Language Level.*/
+    private ArrayList<LanguageLevel> LanguageLevelStore() {
+
+        ArrayList<LanguageLevel> level = new ArrayList<LanguageLevel>();
+
+        level.add(LanguageLevel.BEGINNER);
+        level.add(LanguageLevel.INTERMEDIATE);
+        level.add(LanguageLevel.ADVANCED);
+        level.add(LanguageLevel.FLUENT);
+        level.add(LanguageLevel.NATIVE);
+
+        return level;
+    }
+}
--- src/main/java/fr/dta/ovg/services/user/UserCrudServiceImpl.java
+++ src/main/java/fr/dta/ovg/services/user/UserCrudServiceImpl.java
@@ -7,7 +7,6 @@ package fr.dta.ovg.services.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -36,7 +35,7 @@ public class UserCrudServiceImpl implements UserCrudService {
     @Override
     public Page<User> getAll(final Pageable pageable, final String search) {
         final Page<User> paginateResult;
-        
+
         if (search == null) {
             paginateResult = this.repository.findAll(pageable);
         } else {
