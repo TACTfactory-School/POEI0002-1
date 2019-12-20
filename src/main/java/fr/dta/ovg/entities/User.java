c1ab79014095c49e38d6db9a98cf0c063c416fc6
Fabrice Reigner <fabricereigner49@msn.com>
2019-12-20 13:51:52 +0100

    fix checkstyle consideration

--- src/main/java/fr/dta/ovg/entities/User.java
+++ src/main/java/fr/dta/ovg/entities/User.java
@@ -19,7 +19,6 @@ import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
