480871d781fce82848bcd9d5f49603fb66dfcc27
Fabrice Reigner <fabricereigner49@msn.com>
2019-12-29 17:05:39 +0100

    fix user test and add test unit for event

--- src/test/java/fr/dta/ovg/UserControllerTest.java
+++ src/test/java/fr/dta/ovg/UserControllerTest.java
@@ -2,28 +2,21 @@ package fr.dta.ovg;
 
 //import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.hamcrest.Matchers.hasSize;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.util.Optional;
 
 import javax.transaction.Transactional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.http.MediaType;
-//import org.springframework.test.annotation.Rollback;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.MockMvc;
 
 import fr.dta.ovg.controllers.UserController;
-import fr.dta.ovg.entities.User;
-import fr.dta.ovg.repositories.UserRepository;
 
 @Transactional
 public class UserControllerTest extends UnitTestBase {
@@ -31,11 +24,11 @@ public class UserControllerTest extends UnitTestBase {
     @Autowired
     private MockMvc mvc;
 
-    @Autowired
-    private UserRepository repository;
+//    @Autowired
+//    private UserRepository repository;
 
-//    private static final String CREATE_BODY = "{\"username\": \"fzedeafrzaxxzafea\", \"birthdate\": \"1989-10-10\","
-//            + " \"email\": \"fzedeafrzaxxzafeaa3@gmail.com\", \"password\": \"fucku\"}";
+    private static final String CREATE_BODY = "{\"username\": \"fzedeafrzaxxzafea\", \"birthdate\": \"1989-10-10\","
+            + " \"email\": \"fzedeafrzaxxzafeaa3@gmail.com\", \"password\": \"fucku\"}";
 
     @Test
     public void testGetAll() throws Exception {
@@ -69,28 +62,38 @@ public class UserControllerTest extends UnitTestBase {
             .andExpect(jsonPath("$.content[0].username").value("Pamwamba"));
     }
 
-//    /**
-//     * @throws Exception
-//     */
+    /**
+     * @throws Exception
+     */
+    @Rollback
+    @Test
+    public void testCreate() throws Exception {
+        this.mvc
+            .perform(post("/api/v1/user")
+                    .content(CREATE_BODY)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isCreated())
+            .andExpect(jsonPath("$.username").value("fzedeafrzaxxzafea"))
+            .andExpect(jsonPath("$.password").doesNotExist());
+    }
+
 //    @Rollback
 //    @Test
-//    public void testCreate() throws Exception {
-//        this.mvc
-//            .perform(post("/api/v1/user")
-//                    .content(CREATE_BODY)
-//                    .contentType(MediaType.APPLICATION_JSON)
-//                    .accept(MediaType.APPLICATION_JSON))
-//            .andExpect(status().isCreated())
-//            .andExpect(jsonPath("$.username").value("fzedeafrzaxxzafea"))
-//            .andExpect(jsonPath("$.password").doesNotExist());
+//    public void testDelete() throws Exception {
+//        this.mvc.perform(delete("/api/v1/user/id"))
+//            .andExpect(status().isNotFound())
+//            .andExpect(jsonPath("$.id").value(1));
+//
 //    }
 
     // TODO: NE devrait PAS etre la !
-    @Test
-    public void foundByUsername() throws Exception {
-        final Optional<User> found = this.repository.findByUsername("Pamwamba");
+//    @Test
+//    public void foundByUsername() throws Exception {
+//        final Optional<User> found = this.repository.findByUsername("Pamwamba");
+//
+//        assertTrue(found.isPresent());
+//        assertEquals("Pamwamba", found.get().getUsername());
+//    }
 
-        assertTrue(found.isPresent());
-        assertEquals("Pamwamba", found.get().getUsername());
-    }
 }
