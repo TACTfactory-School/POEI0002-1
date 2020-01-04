f8f841256e9d77a1f496ae635c6424e56d5852f5
Fabrice Reigner <fabricereigner49@msn.com>
2020-01-04 18:04:35 +0100

    add message unit test

--- src/test/java/fr/dta/ovg/MessageControllerTest.java
+++ src/test/java/fr/dta/ovg/MessageControllerTest.java
@@ -1,5 +1,34 @@
 package fr.dta.ovg;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import javax.transaction.Transactional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.web.servlet.MockMvc;
+
+@Transactional
 public class MessageControllerTest extends UnitTestBase {
 
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void getOneMessageTest() throws Exception {
+        this.mvc.perform(get("/api/v1/message/1"))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.message").value("Salut Pamwamba, Comment tu vas ?"));
+    }
+
+    @Rollback
+    @Test
+    public void testDelete() throws Exception {
+        this.mvc.perform(delete("/api/v1/message/1"))
+            .andExpect(status().isNoContent());
+    }
 }
